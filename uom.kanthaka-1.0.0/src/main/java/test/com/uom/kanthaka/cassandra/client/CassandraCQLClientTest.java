7684046754c77478707447691199a1ec5775e8ab
pushpalankajaya@gmail.com <pushpalankajaya@gmail.com@8cce9ba0-5a94-8d57-41c6-3f9b846a154d>
2012-08-17 14:14:54 +0000

    Test-cases writing initiation.

---
+++ uom.kanthaka-1.0.0/src/main/java/test/com/uom/kanthaka/cassandra/client/CassandraCQLClientTest.java
@@ -0,0 +1,56 @@
+package test.com.uom.kanthaka.cassandra.client;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * CassandraCQLClient Tester.
+ * 
+ * @author <Authors name>
+ * @since <pre>
+ * Aug 17, 2012
+ * </pre>
+ * @version 1.0
+ */
+public class CassandraCQLClientTest {
+
+  @Before
+  public void before() throws Exception {
+  }
+
+  @After
+  public void after() throws Exception {
+  }
+
+  /**
+   * 
+   * Method: main(String[] args)
+   * 
+   */
+  @Test
+  public void testMain() throws Exception {
+    // TODO: Test goes here...
+  }
+
+  /**
+   * 
+   * Method: getLastColumnValue(String columnFamily, String rowKey)
+   * 
+   */
+  @Test
+  public void testGetLastColumnValue() throws Exception {
+    // TODO: Test goes here...
+  }
+
+  /**
+   * 
+   * Method: Hcl()
+   * 
+   */
+  @Test
+  public void testHcl() throws Exception {
+    // TODO: Test goes here...
+  }
+
+}
---
+++ uom.kanthaka-1.0.0/src/main/java/test/com/uom/kanthaka/cassandra/ruleexecuter/SliceQuerySampleTest.java
@@ -0,0 +1,95 @@
+package test.com.uom.kanthaka.cassandra.ruleexecuter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * SliceQuerySample Tester.
+ * 
+ * @author <Authors name>
+ * @since <pre>
+ * Aug 17, 2012
+ * </pre>
+ * @version 1.0
+ */
+public class SliceQuerySampleTest {
+
+  @Before
+  public void before() throws Exception {
+  }
+
+  @After
+  public void after() throws Exception {
+  }
+
+  /**
+   * 
+   * Method: createKeyspace()
+   * 
+   */
+  @Test
+  public void testCreateKeyspace() throws Exception {
+    // TODO: Test goes here...
+  }
+
+  /**
+   * 
+   * Method: createTable(String tableName)
+   * 
+   */
+  @Test
+  public void testCreateTable() throws Exception {
+    // TODO: Test goes here...
+  }
+
+  /**
+   * 
+   * Method: indexColumn(String idxColumnName, String cfName, String columnType)
+   * 
+   */
+  @Test
+  public void testIndexColumn() throws Exception {
+    // TODO: Test goes here...
+  }
+
+  /**
+   * 
+   * Method: getValueInCell(String tableName, String rowID, String columnID)
+   * 
+   */
+  @Test
+  public void testGetValueInCell() throws Exception {
+    // TODO: Test goes here...
+  }
+
+  /**
+   * 
+   * Method: main(String[] args)
+   * 
+   */
+  @Test
+  public void testMain() throws Exception {
+    // TODO: Test goes here...
+  }
+
+  /**
+   * 
+   * Method: dataInserter(String ruleName, String columnName,
+   * ConcurrentHashMap<String, Long> entries)
+   * 
+   */
+  @Test
+  public void testDataInserter() throws Exception {
+    // TODO: Test goes here...
+    /*
+     * try { Method method =
+     * SliceQuerySample.getClass().getMethod("dataInserter", String.class,
+     * String.class, ConcurrentHashMap<String,.class);
+     * method.setAccessible(true); method.invoke(<Object>, <Parameters>); }
+     * catch(NoSuchMethodException e) { } catch(IllegalAccessException e) { }
+     * catch(InvocationTargetException e) { }
+     */
+  }
+
+}
